91f5dd91ee8e0eae0f2664821e4f66fb75b6af38
luson <luson@163.com>
2024-11-06 11:33:37 +0800

    update

--- packages/pofresh/template/game-server/app/servers/connector/handler/entryHandler.js
+++ packages/pofresh/template/game-server/app/servers/connector/handler/entryHandler.js
@@ -2,50 +2,53 @@ module.exports = function (app) {
     return new Handler(app);
 };
 
-let Handler = function (app) {
-    this.app = app;
-};
 
-/**
- * New client entry.
- *
- * @param  {Object}   msg     request message
- * @param  {Object}   session current session object
- * @param  {Function} next    next step callback
- * @return {Void}
- */
-Handler.prototype.entry = function (msg, session, next) {
-    next(null, {code: 200, msg: 'game server is ok.'});
-};
+class Handler {
+    constructor(app) {
+        this.app = app;
+    }
 
-/**
- * Publish route for mqtt connector.
- *
- * @param  {Object}   msg     request message
- * @param  {Object}   session current session object
- * @param  {Function} next    next step callback
- * @return {Void}
- */
-Handler.prototype.publish = function (msg, session, next) {
-    let result = {
-        topic: 'publish',
-        payload: JSON.stringify({code: 200, msg: 'publish message is ok.'})
-    };
-    next(null, result);
-};
+    /**
+     * New client entry.
+     *
+     * @param  {Object}   msg     request message
+     * @param  {Object}   session current session object
+     * @param  {Function} next    next step callback
+     * @return {Void}
+     */
+    entry(msg, session, next) {
+        next(null, {code: 200, msg: 'game server is ok.'});
+    }
 
-/**
- * Subscribe route for mqtt connector.
- *
- * @param  {Object}   msg     request message
- * @param  {Object}   session current session object
- * @param  {Function} next    next step callback
- * @return {Void}
- */
-Handler.prototype.subscribe = function (msg, session, next) {
-    let result = {
-        topic: 'subscribe',
-        payload: JSON.stringify({code: 200, msg: 'subscribe message is ok.'})
-    };
-    next(null, result);
-};
+    /**
+     * Publish route for mqtt connector.
+     *
+     * @param  {Object}   msg     request message
+     * @param  {Object}   session current session object
+     * @param  {Function} next    next step callback
+     * @return {Void}
+     */
+    publish(msg, session, next) {
+        let result = {
+            topic: 'publish',
+            payload: JSON.stringify({code: 200, msg: 'publish message is ok.'})
+        };
+        next(null, result);
+    }
+
+    /**
+     * Subscribe route for mqtt connector.
+     *
+     * @param  {Object}   msg     request message
+     * @param  {Object}   session current session object
+     * @param  {Function} next    next step callback
+     * @return {Void}
+     */
+    subscribe(msg, session, next) {
+        let result = {
+            topic: 'subscribe',
+            payload: JSON.stringify({code: 200, msg: 'subscribe message is ok.'})
+        };
+        next(null, result);
+    }
+}
